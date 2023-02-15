68d430bce122c704faeaf3e0ca535a2de105a916
Leslie Pino <contactlespino@gmail.com>
2023-02-15 11:29:46 +0800

    Added 404 Empty State UI.

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,17 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import App from './App';
 import About from './pages/About';
 import './css/globals.css';
+import NotFound from './components/empty UI state/NotFound';
 
 ReactDOM.render(
   <BrowserRouter>
     <Routes>
       <Route path='/' element={<App />} />
+      <Route path='/home' element={<App />} />
       <Route path='/about' element={<About />} />
+      <Route path='/*' element={<NotFound />} />
     </Routes>
   </BrowserRouter>,
   document.getElementById('root')
