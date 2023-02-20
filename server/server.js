6e3328852315903ec03585b2d88c49ac2f24c241
Leslie Pino <contactlespino@gmail.com>
2023-02-20 09:47:19 +0800

    added cors

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv'
 import apiRouter from './routes/newsRoutes.js';
 import apiKeyRouter from './routes/apiKey.js';
+import cors from 'cors';
 
 dotenv.config();
 const app = express();
