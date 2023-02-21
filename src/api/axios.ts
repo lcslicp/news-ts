bae9122d0e60c3270adf8cdec067fdaffe9bbaba
Leslie Pino <contactlespino@gmail.com>
2023-02-21 21:32:20 +0800

    configured for deployment

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: 'http://localhost:5000/api',
+    baseURL: 'https://nownews-server.onrender.com/demoapi',
     withCredentials: false,
 });
