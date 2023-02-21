f407c79aff0258a3dd7c40c9d0b410826f281eec
Leslie Pino <contactlespino@gmail.com>
2023-02-21 22:20:40 +0800

    Configured for deployment.

--- server/server.js
+++ server/server.js
@@ -7,10 +7,15 @@ dotenv.config();
 const app = express();
 app.use(cors());
 
+app.use(cors({
+  credentials: true,
+  origin: ['https://nownewsdemo.netlify.app']
+}));
+
 app.use(express.json());
 
 app.use(function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
+  res.header('Access-Control-Allow-Origin', 'https://nownewsdemo.netlify.app');
   res.header('Access-Control-Allow-Credentials', false);
   next();
 });
@@ -18,6 +23,8 @@ app.use(function (req, res, next) {
 app.get('/api/data', (req, res) => {
   res.json(data);
 });
+
+app.get('/', (req, res) => { res.send('Hello from Backend!')});
 
 const port = process.env.PORT || 5000;
 
