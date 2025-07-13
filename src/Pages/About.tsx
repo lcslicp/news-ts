1a1c46b90d684db37aac023641699c114b6915e8
Leslie P. <contactlespino@gmail.com>
2025-07-13 11:33:19 +0800

    Updated About page copy.

--- src/Pages/About.tsx
+++ src/Pages/About.tsx
@@ -29,7 +29,7 @@ const About = () => {
           as a personal project.
           <br />
           <br />
-          <em>NowNews</em> is not a real media entity or business; it is solely
+          <em>The Modern Newspaper</em> is not a real media entity or business; it is solely
           created for the purpose of this personal project.
         </p>
         <br />
@@ -58,11 +58,6 @@ const About = () => {
           reached, the website will no longer be able to fetch data from the
           API. By using this site, you agree to the terms and conditions set
           forth by News API.
-          <br />
-          <br />
-          If you are interested in testing out the user interface of the website
-          without the limitations set upon by the API used, you may visit the
-          <a href='https://nownewsdemo.lcslicp.dev/' target='_blank'> Demo Site</a> that uses placeholder content.
         </p>
         </div>
       </section>
